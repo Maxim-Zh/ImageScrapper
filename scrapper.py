326e6c589002aaa6e4277e296c5ea162555e4e28
Maksim Zheleznov <zheleznov.maxim@gmail.com>
2021-08-27 11:39:38 +0300

    Divided get_img_urls() into scrape_google() and scrape_yandex() for better readability

--- scrapper.py
+++ scrapper.py
@@ -5,11 +5,14 @@ import io
 import requests
 from PIL import Image
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
 from loggers import info_log, error_log
+
 SE_DICT = {
-    'Google': 'https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img'
+    'Google': 'https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img',
+    'Yandex': 'https://yandex.ru/images/search?text={q}'
 }
 
 
@@ -18,36 +21,52 @@ class ImageScrapper:
     Scraps image from search engine and downloads it to 'Download' dir
     """
 
-    def __init__(self, search_engine: str):
-        #  install webdriver
+    def __init__(self):
+        # install webdriver
         self.opts = webdriver.ChromeOptions()
-        self.opts.headless = True
+        # self.opts.headless = True
+        self.opts.add_argument('start-maximized')
+        self.opts.add_argument('--disable-blink-features=AutomationControlled')
+        self.opts.add_experimental_option('excludeSwitches', ['enable-automation'])
+        self.opts.add_experimental_option('useAutomationExtension', False)
         self.webdriver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=self.opts)
+        self.webdriver.execute_script('Object.defineProperty(navigator, "webdriver", {get: () => undefined})')
+        self.webdriver.execute_cdp_cmd('Network.setUserAgentOverride',
+                                       {
+                                           "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                                                        '(KHTML, like Gecko) '
+                                                        'Chrome/83.0.4103.53 Safari/537.36'})
 
         #  params
-        self.search_engine = search_engine
-        self.search_url = SE_DICT[search_engine]
+        self.search_engine = None
+        self.query = None
         self.img_urls = set()
         self.img_count = 0
         self.result_start = 0
-        self.query = None
         self.sub_dir_name = ''
 
-    def get_img_urls(self, query: str, max_urls: int, sleep: int = 1) -> set:
+    def scroll_to_end(self, sleep: int = 2) -> None:
+        """
+        Scrolls to the bottom
+
+        :param sleep: wait between interactions
+        :return: None
+        """
+        self.webdriver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        time.sleep(sleep)
+
+    def scrape_google(self, query: str, max_urls: int, sleep: int = 2) -> set:
         """
         Search Google for images by given query, return set of image urls
 
         :param query: what to search
         :param max_urls: number of images
-        :param sleep: sleep between interactions
+        :param sleep: wait between interactions
         :return: set of urls
         """
-
-        def scroll_to_end():
-            self.webdriver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-            time.sleep(sleep)
-
-        self.webdriver.get(url=self.search_url.format(q=query))
+        self.search_engine = 'Google'
+        search_url = SE_DICT[self.search_engine]
+        self.webdriver.get(url=search_url.format(q=query))
         self.query = query
 
         for char in self.query:
@@ -57,7 +76,7 @@ class ImageScrapper:
                 self.sub_dir_name += '_'
 
         while self.img_count < max_urls:
-            scroll_to_end()
+            self.scroll_to_end()
 
             #  find all img tags
             thumbnail_imgs = self.webdriver.find_elements_by_css_selector('img.Q4LuWd')
@@ -71,7 +90,7 @@ class ImageScrapper:
                     thumbnail_img.click()
                     time.sleep(sleep)
                 except Exception as err:
-                    error_log.exception(err)
+                    error_log.exception(f'{err}\n')
                     continue
 
                 #  get full img url
@@ -94,11 +113,55 @@ class ImageScrapper:
                 if load_more_button:
                     self.webdriver.execute_script('document.querySelector(".mye4qd").click();')
             self.result_start = len(thumbnail_imgs)
+        # close browser
         self.webdriver.quit()
-        info_log.info('Success')
         return self.img_urls
 
-    def download_image(self):
+    def scrape_yandex(self, query: str, max_urls: int, sleep: int = 2) -> set:
+        """
+        Search Yandex for images by given query, return set of image urls
+
+        :param query: what to search
+        :param max_urls: number of images
+        :param sleep: wait between interactions
+        :return: set of urls
+        """
+        self.search_engine = 'Yandex'
+        search_url = SE_DICT[self.search_engine]
+        self.webdriver.get(url=search_url.format(q=query))
+        self.query = query
+
+        for char in self.query:
+            if char not in r'<>:"/\|?*':
+                self.sub_dir_name += char
+            else:
+                self.sub_dir_name += '_'
+
+        while self.img_count < max_urls:
+            time.sleep(sleep)
+            #  try clicking on first image
+            thumbnail_img = self.webdriver.find_element_by_css_selector('div.serp-item__preview')
+            try:
+                thumbnail_img.click()
+                html_elem = self.webdriver.find_element_by_tag_name('html')  # where to send ARROW_DOWN key call
+                for _ in range(max_urls):
+                    time.sleep(sleep)
+                    full_img = self.webdriver.find_element_by_css_selector('img.MMImage-Origin')
+                    if full_img.get_attribute('src') and 'http' in full_img.get_attribute('src'):
+                        self.img_urls.add(full_img.get_attribute('src'))
+                    self.img_count = len(self.img_urls)
+                    html_elem.send_keys(Keys.ARROW_DOWN)
+            except Exception as err:
+                error_log.exception(f'{err}\n')
+        # close browser
+        self.webdriver.quit()
+        return self.img_urls
+
+    def download_image(self) -> str or None:
+        """
+        Download images from found urls
+        :return: path to subdir to open it in GUI or None if there if no urls found
+        """
         if self.img_urls:
             #  create dirs
             dir_path = os.path.join(os.path.dirname(__file__), 'Download')
@@ -119,15 +182,16 @@ class ImageScrapper:
                     with open(file=f'{file_path}.jpeg', mode='wb') as file:
                         image.save(file, 'JPEG')
                 except Exception as err:
-                    error_log.exception(f'ERROR downloading {url} - {err}')
-            print(sub_dir_path)
+                    error_log.exception(f'ERROR downloading {url} - {err}\n')
+            info_log.info(f'Successfully downloaded images by query {self.query} from {self.search_engine}\n')
             return sub_dir_path
         else:
-            error_log.exception('No URLs found!')
+            error_log.exception('No URLs found!\n')
             return None
 
 
 if __name__ == '__main__':
-    google_parser = ImageScrapper(search_engine='Google')
-    google_parser.get_img_urls(query='cat', max_urls=1)
-    google_parser.download_image()
+    scrapper = ImageScrapper()
+    scrapper.scrape_google(query='cat', max_urls=2)
+    # scrapper.scrape_yandex(query='cat', max_urls=2)
+    # scrapper.download_image()
