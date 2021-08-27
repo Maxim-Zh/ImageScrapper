6ba53123f639aa246bd15b68c3adfa44c8acb70f
Maksim Zheleznov <zheleznov.maxim@gmail.com>
2021-08-27 11:42:37 +0300

    typo fixes

--- scrapper.py
+++ scrapper.py
@@ -79,13 +79,13 @@ class ImageScrapper:
             self.scroll_to_end()
 
             #  find all img tags
-            thumbnail_imgs = self.webdriver.find_elements_by_css_selector('img.Q4LuWd')
-            thumbnail_img_count = len(thumbnail_imgs)
+            thumbnail_images = self.webdriver.find_elements_by_css_selector('img.Q4LuWd')
+            thumbnail_img_count = len(thumbnail_images)
             info_log.info(f'Found {thumbnail_img_count} thumbnail images! '
                           f'Extracting links from {self.result_start}:{thumbnail_img_count}...')
 
             #  try clicking on thumbnail
-            for thumbnail_img in thumbnail_imgs[self.result_start:thumbnail_img_count]:
+            for thumbnail_img in thumbnail_images[self.result_start:thumbnail_img_count]:
                 try:
                     thumbnail_img.click()
                     time.sleep(sleep)
@@ -94,8 +94,8 @@ class ImageScrapper:
                     continue
 
                 #  get full img url
-                full_imgs = self.webdriver.find_elements_by_css_selector('img.n3VNCb')
-                for full_img in full_imgs:
+                full_images = self.webdriver.find_elements_by_css_selector('img.n3VNCb')
+                for full_img in full_images:
                     if full_img.get_attribute('src') and 'http' in full_img.get_attribute('src'):
                         self.img_urls.add(full_img.get_attribute('src'))
                 self.img_count = len(self.img_urls)
@@ -112,7 +112,7 @@ class ImageScrapper:
                 load_more_button = self.webdriver.find_element_by_css_selector('.mye4qd')
                 if load_more_button:
                     self.webdriver.execute_script('document.querySelector(".mye4qd").click();')
-            self.result_start = len(thumbnail_imgs)
+            self.result_start = len(thumbnail_images)
         # close browser
         self.webdriver.quit()
         return self.img_urls
@@ -183,7 +183,7 @@ class ImageScrapper:
                         image.save(file, 'JPEG')
                 except Exception as err:
                     error_log.exception(f'ERROR downloading {url} - {err}\n')
-            info_log.info(f'Successfully downloaded images by query {self.query} from {self.search_engine}\n')
+            info_log.info(f'Successfully downloaded images by query "{self.query}" from {self.search_engine}\n')
             return sub_dir_path
         else:
             error_log.exception('No URLs found!\n')
